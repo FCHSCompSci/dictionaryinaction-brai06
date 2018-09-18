36f6cbb5d4c41eb4595e572ee8a2296b0d767ccc
Rai <bijaya.rai@stu.jefferson.kyschools.us>
2018-09-18 10:38:15 -0400

    comment

---
+++ Dictionaries.py
@@ -0,0 +1,16 @@
+
+car = {
+    'car_1': "Mustang"
+    'car_2': "Nissan"
+    'car_3': "Toyota"
+    'car_4': "Honda"
+    'car_5': "BMW"
+    'car_6': "Lamborgini"
+    'car_7': "Ferrari"
+    'car_8': "Mercedez Benze"
+    'car_9': "Bugatti"
+    'car_10': "T20"
+    }
+while True:
+    Question_1 = ("Do you wanna buy a car? ")
+    
