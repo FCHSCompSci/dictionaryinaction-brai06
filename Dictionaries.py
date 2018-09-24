6023e7345d9cf263bd2f49250aa46494f22cff12
Rai <bijaya.rai@stu.jefferson.kyschools.us>
2018-09-24 10:14:31 -0400

    Name: Bijaya Rai Date: 9/24/18 Project Title: Dictionaries

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,16 +1,20 @@
-
 car = {
-    'car_1': "Mustang"
-    'car_2': "Nissan"
-    'car_3': "Toyota"
-    'car_4': "Honda"
-    'car_5': "BMW"
-    'car_6': "Lamborgini"
-    'car_7': "Ferrari"
-    'car_8': "Mercedez Benze"
-    'car_9': "Bugatti"
-    'car_10': "T20"
+    'type:': "type",
+    'color:': "color",
+    'amount:': "amount"
     }
 while True:
-    Question_1 = ("Do you wanna buy a car? ")
-    
+    print("Welcome to Forever Car Dealer")
+    car_buyer = input("Would you like to [s]hop,[p]ick up or [l]eave? ")
+    if car_buyer == 's':
+        car_type = input("What type of car would you prefer? ")
+        car_color = input("What color would you prefer? ")
+        car_amount = input("Hom many cars do you want to buy? ")
+        car['type:'] = car_type
+        car['color:'] = car_color
+        car['amount:'] = car_amount
+    elif car_buyer == 'p':
+        for k,v in car.items():
+            print(k,v)
+    else:
+        break
